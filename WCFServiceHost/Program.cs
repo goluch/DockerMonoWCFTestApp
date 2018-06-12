fa780ef4e68e5a8a5f996e915721f26e22fc2035
dblab <dblab@komp101.nmkzllab>
2018-06-12 12:46:07 +0200

    first commit2

--- WCFServiceHost/Program.cs
+++ WCFServiceHost/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WcfServiceLibrary;
 
 namespace WCFServiceHost
 {
@@ -10,6 +12,12 @@ namespace WCFServiceHost
     {
         static void Main(string[] args)
         {
+            var host = new ServiceHost(typeof(Service1)/*, new Uri[] { new Uri("http://localhost:1100") }*/);
+            host.AddServiceEndpoint(typeof(IService1), new NetTcpBinding(), "net.tcp://localhost/wcftcp");
+
+            host.Open();
+            Console.ReadKey();
+            host.Close();
         }
     }
 }
